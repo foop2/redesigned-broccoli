326106cb70585d29e8ea09ba5774aa8b863eb791
foop <104875433+foop2@users.noreply.github.com>
2024-11-05 15:47:24 -0500

    made pause/unpause feature (same key)
    i copied the code from p5.js and put it here. the way i did it was by having a variable, if its negative do things if its not, do nothing, and pressing the p key multiplies it by negative one and flips the value. like this:
    
    if (key that was pressed === p) {
      isPaused  * -1  //flips from negative to positive or vice versa
    
    if (isPaused === -1) {
      do stuff
    }
    
    if (isPaused === 1) {
      put text on screen saying its paused
    }

--- sketch.js
+++ sketch.js
@@ -1,3 +1,59 @@
+let fluid;
+let isPaused = -1;
+
+function setup() {
+  createCanvas(600, 600);
+  frameRate(5);
+  fluid = new Fluid(0.2, 0, 0.0000001);
+}
+
+
+function draw() {
+  stroke(51);
+  strokeWeight(2);
+
+  //makes text when paused
+  if (isPaused === 1) {
+    push();
+    textAlign(CENTER, TOP);
+    textSize(75);
+    fill('white');
+    strokeWeight(6);
+    stroke('black');
+    text('Paused', width / 2, height / 2)
+    pop();
+  }
+  
+  if (isPaused === -1) {
+    //checks if its paused or not before doing things
+    let cx = int((0.5 * width) / SCALE);
+    let cy = int((0.5 * height) / SCALE);
+    for (let i = -1; i <= 1; i++) {
+      for (let j = -1; j <= 1; j++) {
+        fluid.addDensity(cx + i, cy + j, random(50, 150));
+      }
+    }
+
+    for (let i = 0; i < 2; i++) {
+      let angle = noise(t) * TWO_PI * 2;
+      let v = p5.Vector.fromAngle(angle);
+      v.mult(0.2);
+      t += 0.01;
+     fluid.addVelocity(cx, cy, v.x, v.y);
+    }
+    fluid.step();
+    fluid.renderD();
+  }  
+}
+  
+  //pause/unpause detection feature
+
+  function keyPressed() {
+  if (key === 'p') {
+    isPaused *= -1 //'p' changes -1 to 1, if its -1 simulation plays, if its 1 it does not play
+  }
+}
+
 // Fluid Simulation
 // Daniel Shiffman
 // https://thecodingtrain.com/CodingChallenges/132-fluid-simulation.html
@@ -8,34 +64,3 @@
 // http://www.dgp.toronto.edu/people/stam/reality/Research/pdf/GDC03.pdf
 // Fluid Simulation for Dummies by Mike Ash
 // https://mikeash.com/pyblog/fluid-simulation-for-dummies.html
-
-let fluid;
-
-function setup() {
-  createCanvas(600, 600);
-  frameRate(22);
-  fluid = new Fluid(0.2, 0, 0.0000001);
-}
-
-function draw() {
-  stroke(51);
-  strokeWeight(2);
-
-  let cx = int((0.5 * width) / SCALE);
-  let cy = int((0.5 * height) / SCALE);
-  for (let i = -1; i <= 1; i++) {
-    for (let j = -1; j <= 1; j++) {
-      fluid.addDensity(cx + i, cy + j, random(50, 150));
-    }
-  }
-
-  for (let i = 0; i < 2; i++) {
-    let angle = noise(t) * TWO_PI * 2;
-    let v = p5.Vector.fromAngle(angle);
-    v.mult(0.2);
-    t += 0.01;
-   fluid.addVelocity(cx, cy, v.x, v.y);
-  }
-  fluid.step();
-  fluid.renderD();
-}
